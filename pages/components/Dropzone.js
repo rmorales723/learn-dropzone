cd8f34266876cb5bfe5570e334d85070654f07ee
robert morales <rmoralfit@gmail.com>
2022-06-08 19:35:08 -0400

    created dropzone hook

--- pages/components/Dropzone.js
+++ pages/components/Dropzone.js
@@ -1,9 +1,19 @@
 import React from 'react'
+import {useDropzone} from 'react-dropzone'
+
 
 const Dropzone = () => {
-    return (
-        <div>Dropzone</div>
-    )
+    const onDrop = useCallback(acceptedFiles => {
+        // Do something with the files
+      }, [])
+      const {getRootProps, getInputProps} = useDropzone({onDrop})
+    
+      return (
+        <div {...getRootProps()}>
+          <input {...getInputProps()} />
+              <p>Drop the files here ...</p> :          
+        </div>
+      )
 }
 
 export default Dropzone;
