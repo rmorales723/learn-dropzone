ec6414db663fddc98e48e301431c4012f4a11573
robert morales <rmoralfit@gmail.com>
2022-06-08 19:51:45 -0400

    implemented successful drag and drop

--- pages/components/Dropzone.js
+++ pages/components/Dropzone.js
@@ -1,17 +1,29 @@
-import React from 'react'
+import React, {useCallback, useState} from 'react'
 import {useDropzone} from 'react-dropzone'
 
 
 const Dropzone = () => {
+    const [selectedImages, setSelectedImages] = useState([])
     const onDrop = useCallback(acceptedFiles => {
-        // Do something with the files
+        setSelectedImages(acceptedFiles.map(file=>
+            Object.assign(file,{
+                preview:URL.createObjectURL(file)
+            })
+            ))
       }, [])
       const {getRootProps, getInputProps} = useDropzone({onDrop})
-    
+      const selected_images = selectedImages?.map(file=>(
+          <div>
+              <img src={file.preview} style={{width:"200px"}} alt="" />
+          </div>
+      ))
       return (
+          <div>
         <div {...getRootProps()}>
           <input {...getInputProps()} />
-              <p>Drop the files here ...</p> :          
+              <p>Drop the files here ...</p>           
+        </div>
+        {selected_images}
         </div>
       )
 }
