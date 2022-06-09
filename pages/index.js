3ba575c54f34e5f49456d202245f872b3b9742e6
robert morales <rmoralfit@gmail.com>
2022-06-09 12:24:12 -0400

    successful implementing tailwind css screen

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,17 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import styles from '../styles/Home.module.css'
+
 import Dropzone from './components/Dropzone'
+import Posts from './components/Posts'
 
 export default function Home() {
   return (
-    <div>
+    <div className="flex">
+      <div className='p-5'>
       <Dropzone/>
+      </div>
+      <div className= "p-5 bg-gray-100 min-h-screen w-full">
+        <Posts/>
     </div>
+  </div>
+
   )
 }
