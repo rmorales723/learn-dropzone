c5531f320504ff05ea80a04599d5f240308d801f
robert morales <rmoralfit@gmail.com>
2022-06-09 15:07:30 -0400

    successfully displayed caption to browser from database

--- pages/components/Posts.js
+++ pages/components/Posts.js
@@ -1,9 +1,27 @@
-import React from 'react'
+import React, {useEffect, useState} from 'react'
+import {db} from "../../firebase";
+import { collection, onSnapshot, orderBy, query } from 'firebase/firestore';
 
 const Posts = () => {
+  const  [posts, setPosts] = useState([])
+  useEffect(() => {
+    const collectionRef = collection(db, "posts");
+    const q = query(collectionRef, orderBy("timestamp", "desc"))
+    const unsubscribe = onSnapshot(q, (querySnapshot) => {
+      setPosts(querySnapshot.docs.map(doc => ({...doc.data(), id: doc.id, timestamp: doc.data().timestamp.toDate().getTime()})))
+    });
+
+    return 
+      unsubscribe
+  },[])
+
   return (
-    <div>Posts</div>
-  )
+    <div>{
+      posts.map(post=><div key={post.id}>
+        <div className='text-lg'>{post.caption}</div>
+      </div>)
+      }</div>)
+        
 }
 
 export default Posts
