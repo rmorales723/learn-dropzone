5da1f60240fa1ca5493e8616e58e00ecc2849aeb
robert morales <rmoralfit@gmail.com>
2022-06-09 15:36:53 -0400

    successfully rendering images to browser

--- pages/components/Posts.js
+++ pages/components/Posts.js
@@ -1,14 +1,23 @@
 import React, {useEffect, useState} from 'react'
 import {db} from "../../firebase";
 import { collection, onSnapshot, orderBy, query } from 'firebase/firestore';
+import Image from 'next/image';
 
 const Posts = () => {
   const  [posts, setPosts] = useState([])
+  const postImages = (post)=>{
+    const post_images = post.images?.map(file=>(
+      <div className='relative w-full h-96'>
+        <Image src={file} layout="fill" objectFit='cover'/>
+        </div>
+    ))
+    return post_images
+  }
   useEffect(() => {
     const collectionRef = collection(db, "posts");
     const q = query(collectionRef, orderBy("timestamp", "desc"))
     const unsubscribe = onSnapshot(q, (querySnapshot) => {
-      setPosts(querySnapshot.docs.map(doc => ({...doc.data(), id: doc.id, timestamp: doc.data().timestamp.toDate().getTime()})))
+      setPosts(querySnapshot.docs.map(doc => ({...doc.data(), id: doc.id, timestamp: doc.data().timestamp?.toDate().getTime()})))
     });
 
     return 
@@ -17,11 +26,20 @@ const Posts = () => {
 
   return (
     <div>{
-      posts.map(post=><div key={post.id}>
-        <div className='text-lg'>{post.caption}</div>
-      </div>)
-      }</div>)
+      posts.map(post=><div key={post.id}
+        className=' bg-white rounded-lg shadow-xl p-8 w-1/2 m-auto mb-4'
+
+      >
         
-}
+        <div className='text-lg'>{post.caption}</div>
+        <div className="flex space-x-3">
+          {postImages(post)}
+          </div>
+      </div>)
+      }
+      </div>
+      
+      )
+    }
 
 export default Posts
